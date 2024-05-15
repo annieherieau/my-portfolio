821acead885ecaed0e684f0143915e43ea025381
annieherieau <annie.herieau@gmail.com>
2024-05-15 11:34:02 +0200

    darkmode context fix

--- src/DarkModeContext.jsx
+++ src/DarkModeContext.jsx
@@ -1,9 +1,6 @@
 import { createContext } from "react";
 
-// const DarkModeContext = createContext(false);
-function getDarkMode(isDarkMode) {
-  return isDarkMode ? true : false;
-}
+
 const DarkModeContext = createContext(false
 );
 
